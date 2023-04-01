4c7657aba3e8ea6e775cbec147ee4e74054582ba
israel diaz <israel.diaz1701@gmail.com>
2023-03-31 22:03:21 -0700

    commit tree 26145c7c0eef2c7bd869b4febd0d858c04635a6e

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+main.remove(main);
+let newHeader = document.createElement('h1');
+console.log(newHeader);
+newHeader.setAttribute('id','victory');
+let textNode = document.createTextNode('YOUR-NAME is the champion');
+
+newHeader.appendChild(textNode);
+
+let containerDiv = document.querySelector('.container');
+containerDiv.appendChild(divElement);
